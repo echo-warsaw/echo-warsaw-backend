160938553aa6ca2ab5a87592059d9eda36569f63
Piotr Szulc <kkragoth@gmail.com>
2017-04-29 15:28:28 +0200

    facebook api basic functionality

---
+++ app/fb/facebook_utils.py
@@ -0,0 +1,48 @@
+import facebook
+
+access_token = ''
+graph = None
+testing = True
+
+
+def get_access_token():
+    '''
+        TODO: force renewing token
+    '''
+    return '129471194266873|o4ltMq1cg05Hn4HQ8Xj8HaKa6ZE'
+
+
+def get_graph():
+    global access_token
+    global graph
+
+    if not access_token:
+        access_token = get_access_token()
+
+    if not graph:
+        graph = facebook.GraphAPI(access_token=access_token)
+
+    return graph
+
+
+def get_fb_ids_of_query(q, q_type='page', only_best=True):
+    graph = get_graph()
+    search_results = graph.request('search', {'q' : q, 'type': q_type})['data']
+
+    if only_best:
+        return search_results[0]['id']
+    else:
+        return [k['id'] for k in search_results]
+
+
+def get_page_feed(id):
+    posts = get_graph().get_connections(id=id, connection_name='posts')['data']
+    return [(p['created_time'], p['message']) for p in posts if 'message' in p]
+
+
+def tests():
+    '''
+        Usage example:
+    '''
+    page_id = get_fb_ids_of_query('teatr rozmaitosci')
+    print(get_page_feed(page_id))
